a1b83cf0ff1c6c8d1892db10fd2198a3a5597436
Harsh Patel <hpatel1959@hotmail.com>
2023-03-07 12:56:22 +0000

    add 2 queries to 2_queries_join for total assignment duration

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT SUM(assignment_submissions.duration) AS total_cohort_duration
+FROM assignment_submissions
+JOIN students 
+ON students.id = assignment_submissions.student_id
+JOIN cohorts 
+ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12';
